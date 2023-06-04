e79590cac5e74197488bf81c28e7d26cf49543a4
dumbwaifu-sal <2450062@students.wits.ac.za>
2023-06-04 21:46:03 +0200

    added about link

--- animations.js
+++ animations.js
@@ -1,12 +1,12 @@
-var i=0,text;
-text = document.getElementByID("about").innerText;
+//var i=0,text;
+//text = document.getElementByID("about").innerText;
 
-document.getElementsByClassID("menuANIM").onmouseover = function(){type()};
+d//ocument.getElementsByClassID("menuANIM").onmouseover = function(){type()};
 
-function type(){
-    if(i<text.length){
-        text += charAt(i);
-        i++;
-        setTimeout(typing,50);
-    }
-}
+//function type(){
+    //if(i<text.length){
+       // text += charAt(i);
+       // i++;
+       //setTimeout(typing,50);
+    //}
+//}
