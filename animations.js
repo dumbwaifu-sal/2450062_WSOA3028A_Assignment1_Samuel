9b516c3950d5022ee05f58555fbd9838857aa403
dumbwaifu-sal <2450062@students.wits.ac.za>
2023-06-05 15:37:40 +0200

    added carousel

--- animations.js
+++ animations.js
@@ -1,7 +1,7 @@
 //var i=0,text;
 //text = document.getElementByID("about").innerText;
 
-d//ocument.getElementsByClassID("menuANIM").onmouseover = function(){type()};
+//document.getElementsByClassID("menuANIM").onmouseover = function(){type()};
 
 //function type(){
     //if(i<text.length){
@@ -10,3 +10,19 @@ d//ocument.getElementsByClassID("menuANIM").onmouseover = function(){type()};
        //setTimeout(typing,50);
     //}
 //}
+
+const carousel = document.querySelector(".carousel");
+
+let isDragStart = false;
+
+const dragStart = (e) =>
+{
+    let isDragStart = true;
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    carousel.scrollLeft = e.pageX;
+}
+carousel.addEventListener("mosudedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
